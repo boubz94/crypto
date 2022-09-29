f6d753623a84d9b3a10fb3f298b5dedfe63a723d
Boubou <abdel.mekchouche@gmail.com>
2022-09-30 00:38:20 +0200

    94

--- src/components/Items/ListItem.tsx
+++ src/components/Items/ListItem.tsx
@@ -3,34 +3,43 @@ import { cryptoOptions, fetchData } from "../../utils/fetchData";
 import axios from "axios";
 const ListItem = () => {
   // useState to store data from the fecth
-  const [crypto, setCrypto] = useState({});
+  const [crypto, setCrypto] = useState([]);
   // init axios client
 
-  useEffect(() => {
-    const requetData = async () => {
-      try {
-        const res = await axios
-          .get("https://coinranking1.p.rapidapi.com/coins", {
-            headers: {
-              "X-RapidAPI-Key":
-                "dca176bb96msh7e3d79fe0f6de15p1f4f58jsn854fd5ab5c00",
-              "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-            },
-          })
-          .then((response) => {
-            return response;
-          });
-        setCrypto(res.data?.coins[0].name);
-        return res;
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    requetData();
-  }, []);
-  console.log(crypto);
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "dca176bb96msh7e3d79fe0f6de15p1f4f58jsn854fd5ab5c00",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
 
-  return <>li</>;
+  const fetchData = () => {
+    fetch(
+      "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=5k-_VTxqtCEI&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setCrypto(response.data))
+      .catch((err) => console.error(err));
+  };
+  console.log(crypto[1]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {Array.isArray(crypto) ? (
+        crypto.map((element) => {
+          return <h2>{element}</h2>;
+        })
+      ) : (
+        <h2>no data</h2>
+      )}
+    </>
+  );
 };
 
 export default ListItem;
