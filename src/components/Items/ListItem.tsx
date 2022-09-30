501a7570d41f629ed2aed65f78f76836da12b518
boubz94 <32883370+boubz94@users.noreply.github.com>
2022-09-30 08:37:14 +0200

    fix data

--- src/components/Items/ListItem.tsx
+++ src/components/Items/ListItem.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { cryptoOptions, fetchData } from "../../utils/fetchData";
 import axios from "axios";
 const ListItem = () => {
   // useState to store data from the fecth
@@ -8,6 +7,16 @@ const ListItem = () => {
 
   const options = {
     method: "GET",
+    url: "https://coinranking1.p.rapidapi.com/coins",
+    params: {
+      referenceCurrencyUuid: "yhjMzLPhuIDl",
+      timePeriod: "24h",
+      "tiers[0]": "1",
+      orderBy: "marketCap",
+      orderDirection: "desc",
+      limit: "50",
+      offset: "0",
+    },
     headers: {
       "X-RapidAPI-Key": "dca176bb96msh7e3d79fe0f6de15p1f4f58jsn854fd5ab5c00",
       "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
@@ -15,31 +24,30 @@ const ListItem = () => {
   };
 
   const fetchData = () => {
-    fetch(
-      "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=5k-_VTxqtCEI&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => setCrypto(response.data))
-      .catch((err) => console.error(err));
+    axios
+      .request(options)
+      .then(function (response) {
+        setCrypto(response.data.data.coins);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
-  console.log(crypto[1]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return (
-    <>
-      {Array.isArray(crypto) ? (
-        crypto.map((element) => {
-          return <h2>{element}</h2>;
-        })
-      ) : (
-        <h2>no data</h2>
-      )}
-    </>
-  );
+  const items = crypto.map((item) => {
+    console.log(item);
+
+    return (
+      <li>
+        {item.name} - {item.symbol}
+      </li>
+    );
+  });
+  return <>{items}</>;
 };
 
 export default ListItem;
